2fd1be51d2967964619bbed37eb33b86229a9d02
ozanyaprak <ozanerhanyaprak@icloud.com>
2023-02-19 19:28:48 +0300

    Add project files.

---
+++ MovieApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string filmBasligi = "film başlığı";
+            string filmAciklama = "film açıklaması";
+            string filmYonetmen = "film yönetmen adı";
+            string[] oyunculuar = { "oyuncu 1", "oyuncu 2" };
+
+            var m = new Movie();
+
+            m.Title = filmBasligi;
+            m.Description = filmAciklama;
+            m.Director = filmYonetmen;
+            m.Casts = oyunculuar;
+
+            return View(m);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    // controller
+    public class MoviesController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
