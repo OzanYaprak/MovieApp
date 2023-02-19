075fd20cf8c3c50dc0778b200f6aa1c346b5af86
ozanyaprak <ozanerhanyaprak@icloud.com>
2023-02-19 19:53:23 +0300

    view düzenlemesi yapıldı

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,13 @@ namespace MovieApp.Web.Controllers
 
         public IActionResult List()
         {
-            return View();
+            var filmListesi = new List<Movie>() 
+            {
+                new Movie {Title="film 1",Description="açıklama 1", Director="Yönetmen 1", Casts=new string[] {"oyuncu 1","oyuncu 2"}},
+                new Movie {Title="film 2",Description="açıklama 2", Director="Yönetmen 2", Casts=new string[] {"oyuncu 1","oyuncu 2"}},
+                new Movie {Title="film 3",Description="açıklama 3", Director="Yönetmen 3", Casts=new string[] {"oyuncu 1","oyuncu 2"}}
+            };
+            return View(filmListesi);
         }
 
         public IActionResult Details()
