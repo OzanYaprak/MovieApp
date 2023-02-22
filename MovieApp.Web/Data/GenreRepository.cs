d596d18b5514b66ce0d58b0f76d794d4d2e1e3fc
ozanyaprak <ozanerhanyaprak@icloud.com>
2023-02-22 15:24:28 +0300

    Repolar oluşturuldu

---
+++ MovieApp.Web/Data/GenreRepository.cs
@@ -0,0 +1,37 @@
+﻿using MovieApp.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieApp.Web.Data
+{
+    public class GenreRepository
+    {
+        private static readonly List<Genre> _genres = null;
+
+        static GenreRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre {GenreID=1,GenreName="Macera"},
+                new Genre {GenreID=2,GenreName="Komedi"},
+                new Genre {GenreID=3,GenreName="Romantik"},
+                new Genre {GenreID=4,GenreName="Savaş"},
+            };
+        }
+
+        public static List<Genre> Genres
+        {
+            get { return _genres; }
+        }
+
+        public static void Add(Genre genre)
+        {
+            _genres.Add(genre);
+        }
+
+        public static Genre GetById(int id)
+        {
+            return _genres.FirstOrDefault(a => a.GenreID == id);
+        }
+    }
+}
---
+++ MovieApp.Web/Data/MovieRepository.cs
@@ -0,0 +1,36 @@
+﻿using MovieApp.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieApp.Web.Data
+{
+    public class MovieRepository
+    {
+        private static readonly List<Movie> _movies = null;
+
+        static MovieRepository()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie {MovieID=1,Title="film 1",Description="açıklama 1", Director="Yönetmen 1", Casts=new string[] {"oyuncu 1","oyuncu 2"}, ImageURL="1.jpg"},
+                new Movie {MovieID=2,Title="film 2",Description="açıklama 2", Director="Yönetmen 2", Casts=new string[] {"oyuncu 1","oyuncu 2"},ImageURL="2.jpg"},
+                new Movie {MovieID=3,Title="film 3",Description="açıklama 3", Director="Yönetmen 3", Casts=new string[] {"oyuncu 1","oyuncu 2"}, ImageURL = "3.jpg"}
+            };
+        }
+
+        public static List<Movie> Movies
+        {
+            get { return _movies; }
+        }
+
+        public static void Add(Movie movie)
+        {
+            _movies.Add(movie);
+        }
+
+        public static Movie GetById(int id)
+        {
+            return _movies.FirstOrDefault(a => a.MovieID == id);
+        }
+    }
+}
