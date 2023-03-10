5fbbb8e4afcd158faeb7c4bfc7c45112d81e2714
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-03-11 01:00:11 +0300

    LINQ Çalışmaları yapıldı.

--- LINQSamples/Program.cs
+++ LINQSamples/Program.cs
@@ -3,20 +3,135 @@
 
 using LINQSamples.Data;
 
+class ProductModel
+{
+    public string Name { get; set; }
+    public decimal? Price { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         using (var db = new NorthwindContext())
         {
-            var products = db.Products.ToList();
+            // Tüm müşteri kayıtlarını getiriniz (Customers)
+            //var customers = db.Customers.ToList();
 
-            foreach (var product in products)
-            {
-                Console.WriteLine(product.ProductName);
-            }
+
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine(customer.ContactName);
+            //}
+
+
+
+            // Tüm müşterilerin sadece CustomerID ve ContactName kolonlarını getiriniz.
+            //var customers = db.Customers.Select(a => new { a.CustomerId, a.ContactName }).ToList();
+
+
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine(customer.CustomerId + " " + customer.ContactName);
+            //}
+
+
+
+            // Almanyada yaşayan müşterilerin adlarını getiriniz.
+            //var customers = db.Customers.Select(a => new { a.ContactName, a.Country }).Where(a => a.Country == "Germany").ToList();
+
+
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine(customer.ContactName + " " + customer.Country);
+            //}
+
+
+
+            // "Diego Roel" isimli müşteri nerede yaşamaktadır ?
+            //var customer = db.Customers.Where(a => a.ContactName == "Diego Roel").FirstOrDefault();
+            //Console.WriteLine(customer.ContactName + " " + customer.Country);
+
+
+
+            // Stokta olmayan ürünler hangileridir ?
+            //var products = db.Products.Select(a => new { a.ProductName, a.UnitsInStock }).Where(a => a.UnitsInStock == 0).ToList();
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product.ProductName + " " + product.UnitsInStock);
+            //}
+
+
+
+            // Tüm çalışanların ad ve soyadlarını tek kolon şeklinde getiriniz.
+            //var employess = db.Employees.Select(a => new { FullName = a.FirstName + " " + a.LastName }).ToList();
+
+            //foreach (var employee in employess)
+            //{
+            //    Console.WriteLine(employee.FullName);
+            //}
+
+
+
+            // Ürünler tablosundaki ilk 5 kaydı alınız.
+            //var products = db.Products.Take(5).ToList();
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product.ProductId + " " + product.ProductName);
+            //}
+
+
+
+            // Ürünler tablosundaki ikinci 5 kaydı alınız. (Take, Skip)
+            //var products = db.Products.Skip(5).Take(5).ToList();
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product.ProductId + " " + product.ProductName);
+            //}
         }
 
         Console.ReadLine();
+    }
+
+
+
+    private static void FiltrelemeSorguları(NorthwindContext db)
+    {
+        //var products = db.Products.Where(a=>a.UnitPrice>18).ToList();
+        //var products = db.Products.Select(a => new { a.ProductName, a.UnitPrice }).Where(a => a.UnitPrice > 18).ToList();
+        //var products = db.Products.Select(a => new { a.ProductName, a.UnitPrice }).Where(a => a.UnitPrice > 18 && a.UnitPrice<30).ToList();
+        //var products = db.Products.Where(a => a.CategoryId == 1).ToList();
+        //var products = db.Products.Where(a => a.CategoryId >= 1 && a.CategoryId <= 5).ToList(); // 1 dahil yazmak istiyorsak < operatörünün yanına = (<=) - (>=) eklememiz gerekiyor !!
+        //var products = db.Products.Where(a => a.CategoryId == 1 || a.CategoryId == 5).ToList();
+        //var products = db.Products.Where(a => a.CategoryId == 1).Select(a => new { a.ProductName, a.UnitPrice }).ToList(); // kategori ıd si 1 olan tabloyu ilk seçmemiz gerekiyor, buna göre daha sonra select yazılıyor.
+        //var products = db.Products.Where(a => a.ProductName=="Chai").ToList();
+        //var products = db.Products.Where(a => a.ProductName.Contains("Ch")).ToList();
+
+
+
+        //foreach (var product in products)
+        //{
+        //    Console.WriteLine(product.ProductName + " " + product.UnitPrice);
+        //}
+    }
+    private static void SeçmeSorguları(NorthwindContext db)
+    {
+        //var products = db.Products.ToList();
+        //var products = db.Products.Select(a => a.ProductName).ToList();
+        //var products = db.Products.Select(a => new {a.ProductName, a.UnitPrice}).ToList();
+        //var products = db.Products.Select(a => new ProductModel { Name = a.ProductName, Price = a.UnitPrice }).ToList();
+
+        //var product = db.Products.First();
+        //var product = db.Products.Select(a => new { a.ProductName, a.UnitPrice }).FirstOrDefault();
+
+        //Console.WriteLine(product.ProductName + "" + product.UnitPrice);
+
+        //foreach (var product in products)
+        //{
+        //    Console.WriteLine(product.Name + "" + product.Price);
+        //}
     }
 }
