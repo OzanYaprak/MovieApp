5ffad97a2003ef7a52c0c11c38146d4f4bca803f
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-03-16 19:06:33 +0300

    Film Lsitesi Hazırlandı

---
+++ MovieApp.Web/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Data;
+using MovieApp.Web.ViewModels;
+using System.Linq;
+
+namespace MovieApp.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly movieContext _context;
+
+        public AdminController(movieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(new AdminMoviesViewModel
+            {
+                Movies = _context.Movies
+                .Include(a=>a.Genres)
+                .Select(a=>new AdminMovieViewModel 
+                {
+                    MovieID = a.MovieID,
+                    Title = a.Title,
+                    ImageURL = a.ImageURL,
+                    Genres = a.Genres
+
+                }).ToList()
+            });
+        }
+    }
+}
---
+++ MovieApp.Web/ViewModels/AdminMoviesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MovieApp.Web.Entity;
+using System.Collections.Generic;
+
+namespace MovieApp.Web.ViewModels
+{
+    public class AdminMoviesViewModel
+    {
+        public List<AdminMovieViewModel> Movies { get; set; }
+    }
+
+
+    public class AdminMovieViewModel
+    {
+        public int MovieID { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public List<Genre> Genres { get; set; }
+    }
+}
